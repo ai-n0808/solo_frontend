430bd733a864d7b0b7ccaa12e90132f9b6d73c5a
ai-n0808 <aichan.88.aichan@gmail.com>
2024-11-04 17:31:29 +0900

    feat: add env and components file for login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState<{ id: number; user_name: string } | null>(
+    null
+  );
+  const [loginView, setLoginView] = useState<boolean>(true);
+
+  //Flip the view
+  const handleLandingPageView = () => {
+    setLoginView((preview) => !preview);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="login-container">
+      {loginView ? (
+        <Login handleLandingPageView={handleLandingPageView} />
+      ) : (
+        <SignUp handleLandingPageView={handleLandingPageView} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Signup.tsx
@@ -0,0 +1,3 @@
+import React, { useState } from "react";
+
+const apiUrl = import.meta.env.VITE_API_URL;
